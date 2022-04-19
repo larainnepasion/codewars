c3ae2a001df500599bd64bad89a391f12f8bd926
La Rainne Pasion <60867488+larainnepasion@users.noreply.github.com>
2022-04-19 17:55:31 -0400

    Update solution for 'This is a problem' kata

--- 8-kyu/"This" is a problem.js
+++ 8-kyu/"This" is a problem.js
@@ -13,3 +13,8 @@ n.firstName //Expected: John
 n.lastName //Expected: Doe
 n.name //Expected: John Doe */
 
+function NameMe(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+    this.name = `${first} ${last}`
+}
